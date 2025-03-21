cb6f4b3fd78f42565090477a355a7ddef0ad9048
fisayo-dev <olufisayobadina@gmail.com>
2025-03-21 23:39:45 +0100

    chore: create export file middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,6 @@
+import arcjetMiddleware from './arcjetMiddleware';
+import authMiddleware from './authMiddleware';
+import errorMiddleware from './errorMiddleware';
+
+export { arcjetMiddleware, authMiddleware, errorMiddleware };
+
